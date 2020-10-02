d4eebc26818180da807235b2acb0a756410b2094
venetia-bird <venetia.bird@.realestate.com.au>
2020-10-02 14:53:55 +1000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rehab-dawg/precache-manifest.1551af53762255443bd4e84f09199f67.js"
+  "/precache-manifest.5be27696b02a300f7b4f1f6679331775.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rehab-dawg/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
--- src/pages/ActiveWalk/index.ts
+++
@@ -1,1 +0,0 @@
-export { ActiveWalk } from './ActiveWalk';
