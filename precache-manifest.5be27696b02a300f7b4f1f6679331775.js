419915b7d83e953ca978a80d06198121a7681d98
venetia-bird <venetia.bird@.realestate.com.au>
2020-10-02 13:47:36 +1000

    Updates

---
+++ precache-manifest.5be27696b02a300f7b4f1f6679331775.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2157f502d92405ff2b7b56a8b6ce9365",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95859afdc869e4870cbe",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4e7a84ab06d1caf16466",
+    "url": "/static/js/2.f331daa3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f331daa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95859afdc869e4870cbe",
+    "url": "/static/js/main.c40f6a92.chunk.js"
+  },
+  {
+    "revision": "82f39f1a11575a506fc8",
+    "url": "/static/js/runtime-main.1845c206.js"
+  }
+]);
--- src/utils/types.ts
+++
@@ -1,9 +0,0 @@
-export interface Walk {
-  startDateTime: number;
-  walkTime: number;
-  finishDateTime?: number;
-  walkName: string;
-}
-export interface State {
-  history: Walk[];
-}
